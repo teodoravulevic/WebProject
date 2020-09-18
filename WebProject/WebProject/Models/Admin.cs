db72592f038d8c2a7a27fac2f6fe2b97502039e0
Teodora Vulevic <vulevict@gmail.com>
2020-09-18 22:57:19 +0200

    + Admin model.

---
+++ WebProject/WebProject/Models/Admin.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebProject.Models.Enums;
+
+namespace WebProject.Models
+{
+    public class Admin : User
+    {
+        public Admin(string username, string password, string name, string surname, string gender, DateTime dateOfBirth, RoleEnum role) 
+            : base(username, password, name, surname, gender, dateOfBirth, role) { }
+    }
+}
